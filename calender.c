bff13092918e9c3b692aad32935a4cd37c2466f2
nothing10101 <95474538+cyrDhanush@users.noreply.github.com>
2021-12-07 09:56:41 +0530

    update optimized

--- calender.c
+++ calender.c
@@ -8,32 +8,40 @@ int daysinyear(int day, int month, int year)
 	for(j=5; j<=year; j++)
 	{
 		days=0;
-		if(j==year) limit=month;
-		else limit=12;
-		//printf("limit %d\n", limit);
-		for(i=1;i<=limit;i++)
+		if(j==year) 
 		{
-		
-				
-			if(i<8 && i!=2)      // upto august
+			
+			limit=month;
+			
+			//printf("limit %d\n", limit);
+			for(i=1;i<=limit;i++)
 			{
-				if(i%2==0) days+=30;
-				
-				else days+=31;
+			
+					
+				if(i<8 && i!=2)      // upto august
+				{
+					if(i%2==0) days+=30;
+					
+					else days+=31;
+				}
+				else if(i!=2 && i>=8){	// after august
+					if(i%2==0) days+=31;
+					
+					else days+=30;
+				}
+				else if(i==2)
+				{
+					if((j%4!=0)||(j%100==0&&j%400!=0)) days+=28;    // leap year calculation
+					else days+=29;
+				}    
+				//printf("i: %d, days: %d\n",i,days);
 			}
-			else if(i!=2 && i>=8){	// after august
-				if(i%2==0) days+=31;
-				
-				else days+=30;
-			}
-			else if(i==2)
-			{
-				if((j%4!=0)||(j%100==0&&j%400!=0)) days+=28;    // leap year calculation
-				else days+=29;
-			}    
-			//printf("i: %d, days: %d\n",i,days);
 		}
 		//printf("j: %d, days: %d yearday: %d\n",j,days, yearday);
+		else {
+			if((j%4!=0)||(j%100==0&&j%400!=0)) days+=365;    // leap year calculation
+					else days+=366;
+		}
 		yearday+=days;
 	
 	}
@@ -56,10 +64,13 @@ void select(int day, int month, int year)
 int main()
 {
 	int day,month,year;
-	while(1){
+	/*while(1){
 	
 	printf("Day: "); scanf("%d", &day);
 	printf("Month: "); scanf("%d", &month);
 	printf("Year: "); scanf("%d", &year);
-	select(day,month,year); }
+	select(day,month,year); }*/
+	
+	select(31,5,2004);
+	return 0;
 }
